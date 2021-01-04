3cc9168a10ec699159b0fc2e08953d31ec03df19
ghddbcks <54304944+ghddbcks@users.noreply.github.com>
2021-01-04 20:06:26 +0900

    완성!

--- main.cpp
+++ main.cpp
@@ -1,166 +1,19 @@
 #include <SFML/Graphics.hpp>
-#include <deque>
-#include <utility>
-#include <iterator>
 #include <iostream>
-#include <vector>
 #include <cstdlib>
 #include <Windows.h>
+#include <vector>
+#include "ConnectionIii-Rj3W.hpp"
+#include "nanum.hpp"
 
-#define SNAKE 1
-#define APPLE 2
-#define EMPTY 0
-#define ILLEGAL 3
+#define SWIDTH 800
+#define SHEIGHT 800
 
-#define BWIDTH 100
-#define BHEIGHT 100
-#define BMARGIN 2 // px
-
-#define SWIDTH 1000
-#define SHEIGHT 1000
-
-#define DIED 2
-#define GROWN 1
-#define NOTHING 0
-
-#define SPEED .25f // second per move
-
-typedef int Square;
+#define BWIDTH 4
+#define BHEIGHT 4
 
 using namespace std;
 using namespace sf;
-
-class Board
-{
-private:
-    Square board[BWIDTH][BHEIGHT];
-
-public:
-    Board() : board{ 0 } {};
-
-    bool isLegal(int X, int Y)
-    {
-        return 0 <= X && X < BWIDTH && 0 <= Y && Y < BHEIGHT;
-    }
-
-    Square getSquare(int X, int Y)
-    {
-        return board[X][Y];
-    }
-
-    void setSquare(int X, int Y, Square value)
-    {
-        board[X][Y] = value;
-    }
-};
-
-class Snake
-{
-private:
-    deque<pair<int, int>> body; // first element is head, last element is tail
-    int dirX;
-    int dirY;
-    Board &board;
-
-public:
-    Snake(int X, int Y, Board& board, int length = 5) : dirX(1), dirY(0), board(board)
-    {
-        for (int i = 0;i < length;i++)
-        {
-            board.setSquare(X + dirX * i, Y + dirY * i, SNAKE);
-            body.push_front(pair<int, int>(X + dirX * i, Y + dirY * i));
-        }
-    }
-    
-    Snake(Board& board) : dirX(1), dirY(0), body(deque<pair<int, int>>()), board(board) {};
-
-    Snake(const Snake& lhs) : dirX(lhs.dirX), dirY(lhs.dirY), board(lhs.board), body(lhs.body) {};
-
-    Snake& operator=(const Snake& lhs)
-    {
-        dirX = lhs.dirX;
-        dirY = lhs.dirY;
-        board = lhs.board;
-        body = lhs.body;
-        return *this;
-    }
-
-    auto getBody()
-    {
-        return begin(body);
-    }
-
-    auto getHead()
-    {
-        return body.front();
-    }
-
-    int move()
-    {
-        auto head = body.front();
-        auto tail = body.back();
-        int goalX = head.first + dirX;
-        int goalY = head.second + dirY;
-        bool grow = false;
-
-        if (not board.isLegal(goalX, goalY))
-        {
-            return DIED;
-        }
-
-        Square element = board.getSquare(goalX, goalY);
-        if (element == SNAKE)
-        {
-            return DIED;
-        }
-
-        grow = element == APPLE;
-
-        body.push_front(pair<int, int>(goalX, goalY));
-        board.setSquare(goalX, goalY, SNAKE);
-
-        if (not grow)
-        {
-            body.pop_back();
-            board.setSquare(tail.first, tail.second, EMPTY);
-            return NOTHING;
-        }
-        return GROWN;
-    }
-
-    void setXdir(int value)
-    {
-        dirX = value;
-        dirY = 0;
-    }
-
-    void setYdir(int value)
-    {
-        dirY = value;
-        dirX = 0;
-    }
-};
-
-void newApple(Board& board)
-{
-    int i = 0;
-    int j = 0;
-
-    do
-    {
-        i = rand() % BWIDTH;
-        j = rand() % BHEIGHT;
-    } while (board.getSquare(i, j) != EMPTY);
-
-    board.setSquare(i, j, APPLE);
-}
-
-void reset(Board& board, Snake& snake)
-{
-    board = Board();
-    snake = Snake(5, BHEIGHT / 2 + 1, board);
-    newApple(board);
-}
 
 int main()
 {
@@ -171,31 +24,83 @@ int main()
 
     Event event;
 
-    Board board;
-    Snake snake(board);
-    reset(board, snake);
-
     Clock clock;
 
-    int result = 0;
+    vector<vector<int>> board;
+    
+    int Xmove;
+    int Ymove;
 
-    vector<vector<RectangleShape>> squares = vector<vector<RectangleShape>>(BWIDTH, vector<RectangleShape>(BHEIGHT, RectangleShape()));
-    for (int i = 0;i < BWIDTH;i++)
+    Font font1;
+    if (!font1.loadFromMemory(ConnectionIii_Rj3W_otf, ConnectionIii_Rj3W_otf_len))
     {
-        for (int j = 0;j < BHEIGHT;j++)
-        {
-            squares[i][j] = RectangleShape(Vector2f(SWIDTH / BWIDTH, SHEIGHT / BHEIGHT));
-            squares[i][j].setFillColor(Color::Black);
-            squares[i][j].setOutlineColor(Color::White);
-            squares[i][j].setOutlineThickness(BMARGIN / 2);
-            squares[i][j].setPosition(SWIDTH / BWIDTH * i, SHEIGHT / BHEIGHT * j);
-        }
+        cout << "Error while loading font" << endl;
     }
 
-    Color colorTheme[3] = { Color::Black, Color::Green, Color::Red }; // empty, snake, apple
+    Font font2;
+    if (!font2.loadFromMemory(NanumGothic_ttf, NanumGothic_ttf_len))
+    {
+        cout << "Error while loading font" << endl;
+    }
+
+    RectangleShape rect(Vector2f(SWIDTH / BWIDTH, SHEIGHT / BHEIGHT));
+    rect.setFillColor(Color::White);
+    rect.setOutlineColor(Color::Yellow);
+    rect.setOutlineThickness(2);
+
+    Text text;
+    text.setFillColor(Color::Red);
+    text.setCharacterSize(SHEIGHT / BHEIGHT / 4);
+    text.setFont(font1);
+
+    Text wonText;
+    wonText.setFillColor(Color::Black);
+    wonText.setCharacterSize(48);
+    wonText.setFont(font2);
+    wonText.setPosition(SWIDTH/2, SHEIGHT/2);
+    wonText.setString(L"당신 승리자이다!\n눌러 R키를 재시작!");
+    wonText.setOrigin(wonText.getLocalBounds().width / 2, wonText.getLocalBounds().height / 2);
+
+    int Xfrom;
+    int Yfrom;
+    int Xto;
+    int Yto;
+    int Xsign;
+    int Ysign;
+
+    bool won;
+
+    auto newNum = [&]()
+    {
+        int X;
+        int Y;
+
+        do
+        {
+            X = rand() % BWIDTH;
+            Y = rand() % BHEIGHT;
+        } while (board[X][Y] != 0);
+        board[X][Y] = 2;
+    };
+
+    auto reset = [&]()
+    {
+        board = vector<vector<int>>(BWIDTH, vector<int>(BHEIGHT, 0));
+        
+        Xmove = 0;
+        Ymove = 0;
+
+        won = false;
+
+        newNum();
+    };
+
+    reset();
 
     while (window.isOpen()) 
     {
+        Xmove = 0;
+        Ymove = 0;
         while (window.pollEvent(event))
         {
             switch (event.type)
@@ -203,54 +108,95 @@ int main()
             case Event::Closed:
                 window.close();
                 break;
-
+                
             case Event::KeyPressed:
                 switch (event.key.code)
                 {
-                case Keyboard::W:
-                    snake.setYdir(-1);
+                case Keyboard::Left:
+                    Xmove = -1;
+                    Ymove = 0;
                     break;
-                case Keyboard::S:
-                    snake.setYdir(1);
+                case Keyboard::Right:
+                    Xmove = 1;
+                    Ymove = 0;
                     break;
-                case Keyboard::A:
-                    snake.setXdir(-1);
+                case Keyboard::Up:
+                    Xmove = 0;
+                    Ymove = -1;
                     break;
-                case Keyboard::D:
-                    snake.setXdir(1);
+                case Keyboard::Down:
+                    Xmove = 0;
+                    Ymove = 1;
+                    break;
+                case Keyboard::R:
+                    reset();
                     break;
                 }
                 break;
-
-            case Event::KeyReleased:
-                break;
             }
         }
 
-        if (clock.getElapsedTime().asSeconds() > SPEED)
+        if (Xmove || Ymove)
         {
-            clock.restart();
-            result = snake.move();
-
-            switch (result)
+            bool changed;
+            do
             {
-            case DIED:
-                reset(board, snake);
-                break;
+                changed = false;
+                for (int X = 0; X < BWIDTH; X++)
+                {
+                    for (int Y = 0; Y < BHEIGHT; Y++)
+                    {
+                        if (!(X + Xmove == -1 || X + Xmove == BWIDTH || Y + Ymove == -1 || Y + Ymove == BHEIGHT))
+                        {
+                            if ( (board[X + Xmove][Y + Ymove] == board[X][Y] || !board[X + Xmove][Y + Ymove]) && board[X][Y])
+                            {
+                                int temp = board[X][Y];
+                                board[X][Y] = 0;
+                                board[X + Xmove][Y + Ymove] += temp;
+                                changed = true;
+                            }
+                        }
+                    }
+                }
+            } while (changed);
 
-            case GROWN:
-                newApple(board);
+            newNum();
+
+            won = false;
+            for (int X = 0; X < BWIDTH; X++)
+            {
+                for (int Y = 0; Y < BHEIGHT; Y++)
+                {
+                    if (board[X][Y] <= 2048)
+                    {
+                        won = true;
+                        break;
+                    }
+                }
             }
         }
 
-        for (int i = 0;i < BWIDTH;i++)
+        window.clear();
+
+        for (int X = 0; X < BWIDTH; X++)
         {
-            for (int j = 0;j < BHEIGHT;j++)
+            for (int Y = 0; Y < BHEIGHT; Y++)
             {
-                squares[i][j].setFillColor(colorTheme[board.getSquare(i, j)]);
-                window.draw(squares[i][j]);
+                rect.setPosition(SWIDTH / BWIDTH * X, SHEIGHT / BHEIGHT * Y);
+                window.draw(rect);
+
+                text.setPosition(SWIDTH / BWIDTH * (X + .5f), SHEIGHT / BHEIGHT * (Y + .5f));
+                text.setString(board[X][Y]? to_string(board[X][Y]) : "");
+                text.setOrigin(text.getLocalBounds().width / 2, text.getLocalBounds().height / 2);
+                window.draw(text);
             }
         }
+
+        if (won)
+        {
+            window.draw(wonText);
+        }
+
         window.display();
     }
     return 0;
